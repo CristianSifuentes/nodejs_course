74866ce3402a4f1e8ff5a1011b665c69ce8dd3e4
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-12-30 11:26:34 -0600

    20173012 CSC: Callbacks

--- esenciales_js/practices.js
+++ esenciales_js/practices.js
@@ -1,7 +1,27 @@
-var Objeto = function(){
- 
+// Programación OO (POO)
+// ES6 class
+
+var Objeto = function() {
+
 }
 
-Object.prototype.Saludar = function(msg){
-    console.log("Saludar:", msg);
+Objeto.prototype.Saludar = function(msg, callback) {
+    console.log("Saludo:", msg)
+    if (typeof callback == 'function') {
+        callback();
+    } else {
+        console.log("callback no es una función");
+    }
 }
+
+// instanciar
+var o = new Objeto()
+o.Saludar("Hola mundo", function() {
+    console.log("callback ejecutado");
+})
+o.Saludar("hey...", 5)
+/*Objeto.prototype.Despedirse = function() {
+    console.log("Adios");
+}
+o.Despedirse()
+*/
