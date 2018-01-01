f1989fa21a21d7bea44be5052004ec175d9b8946
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-01-01 14:40:16 -0600

    20180101 CSC: Funciones con parametros, funciones que regresan funciones

--- esenciales_js/funcional.js
+++ esenciales_js/funcional.js
@@ -7,3 +7,15 @@ var a = [1, 2, 3, 4];
 
 var b = a.map((x) => x * 2);
 console.log(a, b);
+
+//2.- Funciones como parametros, funciones que regresan funciones
+//- reutilización de código
+var suma = function(a, b) { return a + b };
+
+var suma2 = function(a) {
+   return function(b){
+       return a + b;
+   }
+};
+
+console.log(suma(2, 2), suma2(2)(3));
