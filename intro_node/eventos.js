82e9d221c1eb64ab3098afc98598f502b68bbded
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-01-06 21:36:17 -0600

    20180106 CSC: Eventos nodejs

--- intro_node/eventos.js
+++ intro_node/eventos.js
@@ -10,7 +10,7 @@ class Carro extends Eventos {
 
     Arrancar(){
         console.log("El auto arranca");
-        this.emit('arranco',this.contador);
+        this.emit('arranco',this.contador, 1, 2, 3);
         this.contador +=1;
     }
 }
@@ -19,8 +19,8 @@ var carro1 = new Carro();
 
 //Para escuechar eventos, suscribirnos con el método on
 
-carro1.on('arranco', function(c){
-    console.log(c, "El evento fue escuechado, y la secuencia ejecutando");
+carro1.on('arranco', function(c, a, b, c){
+    console.log(c, a, b, c, "El evento fue escuechado, y la secuencia ejecutando");
 });
 
 carro1.Arrancar();
