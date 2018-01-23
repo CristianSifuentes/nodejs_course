8e9a5f9ae93e8bc0e4c4619df2d80b1b8d1d4fb2
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-01-22 21:42:32 -0600

    20180122 CSC: Cliente HTTP

--- cliente/client.js
+++ cliente/client.js
@@ -1,7 +1,16 @@
 //Cliente HTTP 
-
+//github.com
 class Cliente {
+  
+    //Realizar peticiones HTTP para obtener información
+    get(uri){
 
+    }
+
+    //REalizar peticiones HTTP para enviar información 
+    post(uri, data){
+
+    }
 }
 
 module.exports = Cliente;
