e21c7268019a896ef01f6393ee970065ff6a6d49
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-01-25 21:26:15 -0600

    20180125 CSC: Ejemplo

--- cliente/main.js
+++ cliente/main.js
@@ -5,7 +5,7 @@ var Cliente = require("./cliente.js");
 var clienteGithud = new Cliente("api.github.com","443","https");
 
 
-clienteGithud.autenticarBasic("cristian", "");
+clienteGithud.autenticarBasic("CristianSifuentes", "");
 
 clienteGithud.get("/users/CristianSifuentes",
 (respuesta) => {
